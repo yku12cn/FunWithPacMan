3857e25ad37f1be970e102d70c75595f552f5f1b
zhangyu wan <wanzy@bu.edu>
2019-05-01 00:21:44 -0400

    add sarsa pacman

--- src/pacman/entries/pacman/SarsaPacMan.java
+++ src/pacman/entries/pacman/SarsaPacMan.java
@@ -1,19 +1,19 @@
 package pacman.entries.pacman;
 
-
-import java.util.Random;
 import pacman.controllers.Controller;
+import java.util.Random;
 import pacman.game.Game;
 import pacman.game.Constants.MOVE;
 
 
-public class QPacMan extends Controller<MOVE> {
+public class SarsaPacMan extends Controller<MOVE> {
 
 
     /** Initialize the policy. */
-    public QPacMan(FeatureSet proto) {
+    public SarsaPacMan(FeatureSet proto) {
         prototype = proto;
         Qfunction = new QFunction(prototype);
+
     }
 
     /** Prepare for the first move. */
@@ -21,47 +21,52 @@ public class QPacMan extends Controller<MOVE> {
         testMode = test;
         lastScore = 0;
         Qfunction.clearTraces();
+        update = false;
+        delta1 = 0;
+        delta2 = 0;
         evaluateMoves(game);
     }
 
     /** Choose a move. */
     public MOVE getMove(Game game, long timeDue) {
-
         return actions[last];
     }
 
     /** Override the move choice. */
     public void setMove(MOVE move) {
         last = -1;
-        for (int i=0; i<actions.length; i++) {
+        for (int i=0; i<actions.length; i++)
             if (actions[i] == move)
                 last = i;
-        }
     }
 
-    /** Prepare for the next move, and learn if appropriate. */
+    /** Learn if appropriate, and prepare for the next move. */
     public void prepare(Game game) {
 
+        // Do a delayed gradient-descent update
+        if (update) {
+            delta2 = r * Qs[last];
+            Qfunction.updateWeights(alpha * (delta1+delta2));
+        }
         // Eligibility traces
-        if (last != best)
-            Qfunction.clearTraces();
-        else
-            Qfunction.decayTraces(r*lambda);
-
+        Qfunction.decayTraces(r*lambda);
         Qfunction.addTraces(features[last]);
 
         // Q-value correction
         double reward = game.getScore() - lastScore;
         lastScore = game.getScore();
-        double delta = reward - Qs[last];
+        delta1 = reward - Qs[last];
 
-        if (!game.gameOver()) {
+        if (!game.gameOver())
             evaluateMoves(game);
-            delta += (r * Qs[best]);
-        }
 
-        if (!testMode)
-            Qfunction.updateWeights(alpha*delta);
+        if (!testMode) {
+            if (game.gameOver()){
+                Qfunction.updateWeights(alpha*delta1);
+            }
+            else
+                update = true;
+        }
     }
 
     /** Compute predictions for moves in this state. */
@@ -69,25 +74,25 @@ public class QPacMan extends Controller<MOVE> {
 
         actions = game.getPossibleMoves(game.getPacmanCurrentNodeIndex());
         features = new FeatureSet[actions.length];
-        Qs = new double[actions.length];
         best = 0;
-        for (int i=0; i<actions.length; i++) {
+        Qs = new double[actions.length];
+        for (int i=0; i<actions.length; i++){
             features[i] = prototype.extract(game, actions[i]);
             Qs[i] = Qfunction.evaluate(features[i]);
             if (Qs[i] > Qs[best])
                 best = i;
+
         }
-        // Explore or exploit
-        if (!testMode && random.nextDouble() < e) {
+        // explore or exploit
+        if (!testMode && random.nextDouble() < e)
             last = random.nextInt(actions.length);
-        }
-        else{
+        else
             last = best;
-        }
+
     }
 
     private Random random = new Random();
-    private FeatureSet prototype; // Class to use
+    private FeatureSet prototype;
     private QFunction Qfunction; // Learned policy
 
     private MOVE[] actions; // Actions possible in the current state
@@ -98,10 +103,12 @@ public class QPacMan extends Controller<MOVE> {
     private int best; // Index of current best action
     private int last; // Index of action actually being taken
     private boolean testMode;
+    private boolean update;
+    private double delta1; // First part of delayed update: r-Q(s,a)
+    private double delta2; // Second part of delayed update: yQ(s',a')
 
-    private double e = 0.5; // Exploration rate
-    private double alpha = 0.001; // Learning rate
+    private double e = 0.01; // Exploration rate
+    private double alpha = 0.01; // Learning rate
     private double r = 0.999; // Discount rate
-    private double lambda = 0.7; // Backup weighting
+    private double lambda = 0.9; // Backup weighting
 }
-
